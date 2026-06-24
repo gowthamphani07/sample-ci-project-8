18c6dd1e8841581b27be48ddea7399c67665ab34
goutham <gowthamphani07@gmail.com>
2026-06-24 19:45:04 +0530

    Initial commit

---
+++ src/main/java/com/multit/App.java
@@ -0,0 +1,15 @@
+package com.multit;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Maven");
+        System.out.println("This is the simple realworld example....");
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Sum of " + a + " and " + b + " is " + (a+b));
+    }
+}
